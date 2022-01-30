745e31d1d6a5fcd6a359dcca8b46f0668cd2831c
Kasi Raj <kasi.raj@blockchainappfactory.com>
2022-01-30 18:01:04 +0530

    Fix from synk

--- app/controllers/secure_blobs_controller.rb
+++ app/controllers/secure_blobs_controller.rb
@@ -7,7 +7,7 @@ class SecureBlobsController < ActiveStorage::BaseController
     expires_in ActiveStorage.service_urls_expire_in
     document = @blob.attachments.first.record
     if document.is_a?(Document) && (document.is_public? || document.user == current_user)
-      redirect_to @blob.url(disposition: params[:disposition])
+      redirect_to URI.parse(@blob.url(disposition: params[:disposition])).path
     else
       head :unauthorized
     end
